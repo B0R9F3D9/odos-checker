fe8325c458100068f8ff9e318440ef3b4bdbfe73
Уэйд Уилсон <usasupsn@gmail.com>
2024-12-20 00:15:04 +0100

    fix next config & deploy script

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	output: 'export',
 	basePath: '/odos-checker',
+	assetPrefix: '/odos-checker/',
 };
 
 export default nextConfig;
